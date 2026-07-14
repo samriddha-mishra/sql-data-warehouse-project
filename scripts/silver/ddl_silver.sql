8feec8db531bc63036bee27947e202194847dfb3
Samriddha Mishra <107779425+samriddha-mishra@users.noreply.github.com>
2026-07-14 00:15:42 -0700

    Create multiple tables in Silver schema
    This script creates multiple tables in the Silver schema, including customer information, product information, and sales details.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,80 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[crm_cust_info](
+	[cst_id] [int] NULL,
+	[cst_key] [nvarchar](50) NULL,
+	[cst_firstname] [nvarchar](50) NULL,
+	[cst_lastname] [nvarchar](50) NULL,
+	[cst_material_status] [nvarchar](50) NULL,
+	[cst_gndr] [nvarchar](50) NULL,
+	[cst_created_date] [date] NULL
+) ON [PRIMARY]
+GO
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[CUST_AZ12](
+	[CID] [nvarchar](50) NULL,
+	[BDATE] [date] NULL,
+	[GEN] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[LOC_A101](
+	[CID] [nvarchar](50) NULL,
+	[CNTRY] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[prd_info](
+	[prd_id] [int] NULL,
+	[prd_key] [nvarchar](50) NULL,
+	[prd_nm] [nvarchar](50) NULL,
+	[prd_cost] [int] NULL,
+	[prd_line] [nvarchar](50) NULL,
+	[prd_start_dt] [date] NULL,
+	[prd_end_dt] [date] NULL,
+	[cst_id] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[PX_CAT_G1V2](
+	[ID] [nvarchar](50) NULL,
+	[CAT] [nvarchar](50) NULL,
+	[SUBCAT] [nvarchar](50) NULL,
+	[MAINTENANCE] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Silver].[sales_details](
+	[sls_ord_num] [nvarchar](50) NULL,
+	[sls_prd_key] [nvarchar](50) NULL,
+	[sls_cust_id] [int] NULL,
+	[sls_order_dt] [date] NULL,
+	[sls_ship_dt] [date] NULL,
+	[sls_due_dt] [date] NULL,
+	[sls_sales] [int] NULL,
+	[sls_quantity] [int] NULL,
+	[sls_price] [int] NULL
+) ON [PRIMARY]
+GO
